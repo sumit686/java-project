9225514ed1d0457ec05dec8f5baa09f546c2a1f6
sumit <128796718+sumit686@users.noreply.github.com>
2023-09-14 23:41:05 +0530

    Add files via upload
    linear search algorithm

--- Main.java
+++ Main.java
@@ -1,40 +1,37 @@
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    String name;
-    int age;
-    String Bloodgroup;
-    
-   Scanner scanner = new Scanner(System.in);
-  System.out.println("enter your name=");
-    name = scanner.nextLine();
-    System.out.println(name);
-    System.out.println("fill your age=");
-    age=scanner.nextInt();
-    System.out.println(age);
-    System.out.println("enter your Bloodgroup=");
-    Bloodgroup=scanner.next();
-    System.out.println(Bloodgroup);
-    scanner.close();
-System.out.println(name+"\n"+age+"\n"+Bloodgroup);
-    if(age>=20){
-      String Group="RED";
-         System.out.println(Group);
+
+
+public class Main {
+   static boolean linearSearch(int arr[],int key){
+        for(int element :arr){
+            if(element==key){
+                return true;
+
+
+            }
+
+        }
+              return false;
+
+
     }
-    else if(age>=15 && age<20){
-      String Group="Blue";
-      System.out.println(Group);
+    static void print(int arr[]){
+       for(int element:arr){
+           System.out.print(element+" ");
+       }
     }
-    else if(age>=10 && age<15){
-      String Group="Yellow";
-      System.out.println(Group);
-      
-    }
-    else{
-      System.out.println("invalid group");
-    }
-  
-          
-  }
+    public static void main(String[] args) {
+int arr[]={6,8,9,10,67};
+Scanner sc=new Scanner(System.in);
+int key=sc.nextInt();
+boolean found=linearSearch(arr,key);
+if(found){
+    System.out.println("value is present in array ");
 }
+else{
+    System.out.println("Value of array absent");
+}
+print(arr);
+
+    }}
